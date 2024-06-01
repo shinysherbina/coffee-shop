befea57cff7d06903d7923d713e1fe2d4f30fb14
Shiny Sherbina <shinysherbina@gmail.com>
2024-06-01 19:28:33 +0530

    Removed unwanted files AppHeader, and changed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import AppHeader from "./AppHeader";
 import Collection from "./Collection";
 
 function App() {
   return (
     <div className="app">
-      {/* <AppHeader /> */}
       <Collection />
     </div>
   );
